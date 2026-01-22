e1c9bc3c05511edb63de6214337433462ba185bb
doej <doej06456@gmail.com>
2026-01-22 14:17:15 +0100

    chore(hooks): update index exports with new hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,3 +2,7 @@ export { useContracts } from './useContracts';
 export { useBalance, useTransactions } from './useStacks';
 export { useTheme } from './useTheme';
 export { useToast } from './useToast';
+export { default as useForm } from './useForm';
+export { default as useLocalStorage } from './useLocalStorage';
+export { useDebounce, useDebouncedCallback } from './useDebounce';
+export { default as useCopyToClipboard } from './useCopyToClipboard';
