384f455ac35ee715b8261a06ee628182fc0b8527
doej <doej06456@gmail.com>
2026-01-22 13:25:25 +0100

    Update component exports index

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,6 @@
 export { default as Navbar } from './Navbar';
+export { default as Button } from './Button';
+export { default as Card, CardHeader, CardBody, CardFooter } from './Card';
+export { default as Modal, ModalActions } from './Modal';
+export { default as Badge } from './Badge';
+export { default as ProgressBar } from './ProgressBar';
