ed6ce3187135408c1413d596d9560efd1c7fa2e4
doej <doej06456@gmail.com>
2026-01-22 13:34:24 +0100

    Update hooks exports index

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,1 +1,4 @@
 export { useContracts } from './useContracts';
+export { useBalance, useTransactions } from './useStacks';
+export { useTheme } from './useTheme';
+export { useToast } from './useToast';
